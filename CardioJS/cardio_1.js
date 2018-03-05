e0451195559fa0926682f1630f216bbbe6fba36e
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-03-05 17:43:24 +0200

    extra to cardio 1

--- CardioJS/cardio_1.js
+++ CardioJS/cardio_1.js
@@ -55,7 +55,17 @@ function reverseString(str) {
             return revString; 
         */
     // 6. Solution using a reduce function:
-            return str.split('').reduce((revString, char) => char + revString, '');
+        /*    
+        return str.split('').reduce((revString, char) => char + revString, '');
+        */
+    // 7. Solution using a spread operator and a map(): 
+            let arr = [...str];
+            console.log(arr);
+            let newArr = arr.map((_, i, arr) => {
+                return arr[arr.length - 1 - i];
+            });
+            return newArr.join("");
+
 }
 
 
@@ -65,10 +75,24 @@ function reverseString(str) {
 
 function isPallindrome(str) {
 
-    // 1. Solution using the buil-ins
-        const revString = str.split('').reverse().join('');
-        return revString === str;
-}
+    // 1. Solution using the buil-ins:
+        /*
+            const revString = str.split('').reverse().join('');
+            return revString === str;
+        */
+    // 2. Solution using a for loop and an if statement:
+        let isPali = true;
+        let caseStr = str.toLowerCase();
+        let arr = caseStr.split('');
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] !== arr[arr.length - 1 - i]) {
+                isPali = false;
+                break;
+            }
+        }
+        return isPali;
+
+}   
 
 
 // CHALLENGE 3: REVERSE A INTEGER
@@ -114,7 +138,7 @@ function capitalizeLetters(str) {
 
 function maxCharacter(str) {
     // 1. Using an object with key -> value pair and a for in loop:
-
+    /*
         const charMap = {};
         let maxNum = 0;
         let maxChar = '';
@@ -133,7 +157,37 @@ function maxCharacter(str) {
             }
         }
         return maxChar;
+    */
+    // 2. Solution with a Sort and a loop:
+        /*
+            const sortedStr = str.split('').sort();
+            const maxChar = {char: '', count: 0};
+            const currentChar = {char: '', count: 0};
 
+            sortedStr.forEach(char => {
+                if (currentChar.char === char) {
+                    currentChar.count++;
+                } else {
+                    if (maxChar.count < currentChar.count) {
+                        maxChar.char = currentChar.char;
+                        maxChar.count = currentChar.count;
+                    }
+                    currentChar.char = char;
+                    currentChar.count = 0;
+                }
+            });
+            return maxChar.char;
+        */
+    // 3. Solution using forEach sort and map:
+            let letters = {};
+            str
+                .toLowerCase()
+                .split('')
+                .forEach(l => (letters[l] = letters[l] + 1 || 1));
+            const sortedLetters = Object.entries(letters)
+                .sort((a, b) => a[1] - b[1])
+                .map(a => a[0]);
+            return sortedLetters[sortedLetters.length - 1];
 
 }
 
@@ -145,16 +199,38 @@ function maxCharacter(str) {
 
 function fizzBuzz() {
     // 1. Solution using a for loop:
-    for (let i = 1; i <= 100; i++) {
-        if (i % 15 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log('Fizz');
-        } else if ( i % 5 === 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
+    /*
+        for (let i = 1; i <= 100; i++) {
+            if (i % 15 === 0) {
+                console.log("FizzBuzz");
+            } else if (i % 3 === 0) {
+                console.log('Fizz');
+            } else if ( i % 5 === 0) {
+                console.log("Buzz");
+            } else {
+                console.log(i);
+            }
         }
+    */
+    // 2. Solution using a for loop II:
+    /*
+        for (let i = 1; i <= 100; i++) {
+            let output = '';
+            if (i % 3 === 0) {
+                output += 'fizz';
+            }
+            if (i % 5 === 0) {
+                output += 'buzz';
+            }
+            console.log(output || i);
+        }  
+    */
+    // 3. Solution using a for loop III with a array:
+    for (let i = 1; i <= 100; i++) {
+        console.log([
+            (i % 3 === 0 ? 'Fizz' : ''),
+            (i % 5 === 0 ? 'Buzz' : '')
+        ].join('') || i);
     }
 }
 
@@ -166,7 +242,7 @@ function fizzBuzz() {
 //  3. Ex: const output = reverseInt(-521);
 //  4. Ex: const output = capitalizeLetters("i love javaScript");
 //  5. Ex: const output = maxCharacter('javascript');
-
+//  6. Ex: 
 const output = fizzBuzz();
 
 console.log(output);
