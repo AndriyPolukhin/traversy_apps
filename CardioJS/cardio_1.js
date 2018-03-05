818e1143fd76d1431857e4dcde5337a4fb611cc3
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-03-05 15:50:29 +0200

    cardio js 1 end

--- CardioJS/cardio_1.js
+++ CardioJS/cardio_1.js
@@ -63,27 +63,79 @@ function reverseString(str) {
 // Return true if palindrome and false if not
 // ex. isPallindrome('racecar') === 'true', isPallindrome('hello') == false
 
-function isPallindrome(str) {}
+function isPallindrome(str) {
+
+    // 1. Solution using the buil-ins
+        const revString = str.split('').reverse().join('');
+        return revString === str;
+}
 
 
 // CHALLENGE 3: REVERSE A INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) == 125
 
-function reverseInt(init) {}
+function reverseInt(int) {
+    const revString = int.toString().split('').reverse('').join('');
+    return parseInt(revString) * Math.sign(int);
+}
 
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Retrun a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascropt') == 'I Love Javascript'
 
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+    // 1. Using a for loop and built-ins:
+        /*
+            const strArr = str.toLowerCase().split(' ');
+            for (let i = 0; i < strArr.length; i++) {
+                strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+            }
+            return strArr.join(' ');
+        */
+    // 2. Using a map() and buil-ins:
+        /*
+            return str
+                .toLowerCase()
+                .split(' ')
+                .map(word => word[0].toUpperCase() + word.substr(1))
+                .join(' ');
+        */
+    // 3. Using a Regular Expression:
+            return str.replace(/\b[a-z]/gi, char => char.toUpperCase());
+
+
+    }
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
 
-function maxCharacter(str) {}
+function maxCharacter(str) {
+    // 1. Using an object with key -> value pair and a for in loop:
+
+        const charMap = {};
+        let maxNum = 0;
+        let maxChar = '';
+
+        str.split('').forEach(function (char) {
+            charMap[char] ? charMap[char]++ : charMap[char] = 1;
+        });
+        
+        for (let char in charMap) {
+            // debugger; 
+            // node inspect cardio_1.js
+            // c - to continue; repl to evaluate mode;
+            if (charMap[char] > maxNum) {
+                maxNum = charMap[char];
+                maxChar = char;
+            }
+        }
+        return maxChar;
+
+
+}
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers form 1 to 100.
@@ -91,11 +143,33 @@ function maxCharacter(str) {}
 // for multiplies of 5 print "Buzz". For numbers which are 
 // multiplies of both 3 and 5, print "FizzBuzz".
 
-function fizzBuzz() {}
+function fizzBuzz() {
+    // 1. Solution using a for loop:
+    for (let i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if ( i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 
 
 // Call Function
-const output = reverseString('hello');
+//  1. Ex: const output = reverseString('hello');
+//  2. Ex: const output = isPallindrome('madam');
+//  3. Ex: const output = reverseInt(-521);
+//  4. Ex: const output = capitalizeLetters("i love javaScript");
+//  5. Ex: const output = maxCharacter('javascript');
+
+const output = fizzBuzz();
 
 console.log(output);
+
+
+
