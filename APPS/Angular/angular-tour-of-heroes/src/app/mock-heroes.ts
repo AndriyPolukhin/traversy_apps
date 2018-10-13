5aea8453d257919a135c58b7987c913b91f80583
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-10-13 21:29:03 +0300

    added services

--- APPS/Angular/angular-tour-of-heroes/src/app/mock-heroes.ts
+++ APPS/Angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -5,17 +5,11 @@ export const HEROES: Hero[] = [
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
+  { id: 15, name: 'Magenta' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
+  { id: 18, name: 'Dr. IQ' },
   { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 20, name: 'Tornado' },
+
 ];
-
-
-/*
-Copyright 2017-2018 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
