4a839fa0ad5be6a69a36d11d09fcd6cf1f4bedf3
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-10-16 21:43:30 +0300

    basic component and styling

--- APPS/Angular/angularapp/src/app/components/users/users.component.ts
+++ APPS/Angular/angularapp/src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  // Property
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = true;
+
 
   constructor() { }
 
   ngOnInit() {
-    console.log('Init...');
+
+
+    this.users = [
+      {
+        firstName: 'Andriy',
+        lastName: 'Polukhin',
+        age: 33,
+        address: {
+          street: 'Rosengaten',
+          city: 'Hamburg',
+          state: 'DE'
+        },
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00')
+      },
+      {
+        firstName: 'Anastasia',
+        lastName: 'Tsukrova',
+        age: 29,
+        address: {
+          street: 'Rosengaten',
+          city: 'Hamburg',
+          state: 'DE'
+        },
+        isActive: false,
+        registered: new Date('03/11/2017 05:15:00')
+      },
+      {
+        firstName: 'Valentina',
+        lastName: 'Yavorskaya',
+        age: 35,
+        address: {
+          street: 'Avtozavodskaya',
+          city: 'Kiev',
+          state: 'UA'
+        },
+        isActive: true,
+        registered: new Date('20/08/2008 18:30:00')
+      }
+    ];
+
+    this.loaded = true;
+
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
