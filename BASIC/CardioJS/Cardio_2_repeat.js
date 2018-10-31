aa337f0d2f74af56d58f49edc2de0247df1e4c13
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-10-31 23:53:40 +0200

    longest word

--- BASIC/CardioJS/Cardio_2_repeat.js
+++ BASIC/CardioJS/Cardio_2_repeat.js
@@ -2,8 +2,27 @@
 // Return the longest word of a string
 // If more than one longest word , put into an array
 // ex. longestWord('hello, my name is Andriy') === 'hello'
-// ex. logestWord('Hello there, my name is Andriy') === ['hello', 'there']
+// ex. longestWord('Hello there, my name is Andriy') === ['hello', 'there']
 
 function longestWord(sen) {
-  return "Let's Start";
+  // Create filtered array
+  const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+  // craete sotred array
+  const sorted = wordArr.sort(function (a, b) {
+    return b.length - a.length;
+  });
+
+  // If multiple words, put into array
+  const longestWordArr = sorted.filter(function (word) {
+    return word.length === sorted[0].length;
+  });
+
+  console.log(longestWordArr);
+
+
+
 }
+
+longestWord('hello, my name is Andriy') === 'hello';
+longestWord('Hello there, my name is Andriy') === ['hello', 'there'];
