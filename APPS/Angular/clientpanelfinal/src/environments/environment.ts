15b522ff06b98a6fb64a27073465ed6ddbe17615
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-11-15 23:33:58 +0200

    client panel practice

---
+++ APPS/Angular/clientpanelfinal/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC5xMK1Gpt0xBSn-zEQv75b0avx4qj7Wmg",
+    authDomain: "clientpanelprod-a3a7a.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-a3a7a.firebaseio.com",
+    projectId: "clientpanelprod-a3a7a",
+    storageBucket: "clientpanelprod-a3a7a.appspot.com",
+    messagingSenderId: "852918474902"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
