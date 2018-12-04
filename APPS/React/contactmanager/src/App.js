15793e93cf31fb741d794a7f17dc047bbce62158
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-12-04 22:18:24 +0200

    react update

--- APPS/React/contactmanager/src/App.js
+++ APPS/React/contactmanager/src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
-import { Provider } from './context';
-
-
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <Contacts />
-          </div>
-        </div>
-      </Provider>
+      <div className="App">
+        <h1>The App Component</h1>
+      </div>
     );
   }
 }
