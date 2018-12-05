b3719cdbe7a3be7f30141ae384103214e02c2e49
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-12-05 22:44:44 +0200

    updates to contact manger

--- APPS/React/contactmanager/src/App.js
+++ APPS/React/contactmanager/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+
+        <Contact
+          name="Andriy Polukhin"
+          email="andriy.polukhin@gmail.com"
+          phone="093-357-0119"
+        />
+        <Contact
+          name="Anastasia Tsukrova"
+          email="anastasia.tsukrova@gmail.com"
+          phone="480-555-5555"
+        />
+        </div>
       </div>
     );
   }
