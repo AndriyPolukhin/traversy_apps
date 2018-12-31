1b31a677cacf06465cc81742692da61bdfca6223
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-12-31 12:11:05 +0200

    body mass index calculator

--- APPS/React/Projects/04_bodymasscalculator/src/App.js
+++ APPS/React/Projects/04_bodymasscalculator/src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Range from './components/Range';
+import Output from './components/Output';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: 170,
+      weight: 65,
+      bmi: 22.49,
+      bmiClass: 'Normal'
+    }
+  }
+
+  heightChange = (height) => {
+    this.setState({
+      height: height
+    }, this.setBmi)
+  }
+
+  weightChange = (weight) => {
+    this.setState({
+      weight: weight
+    }, this.setBmi)
+  }
+
+  setBmi = () => {
+    let bmi = ((this.state.weight / this.state.height / this.state.height) * 10000).toFixed(2);
+    this.setState({
+      bmi: bmi,
+      bmiClass: this.getBmiClass(bmi)
+    });
+  }
+
+  getBmiClass(bmi) {
+    if (bmi < 18.49) return 'Underweight';
+    if (bmi >= 18.5 && bmi <= 24.99) return 'Normal';
+    if (bmi >= 25 && bmi <= 29.99) return 'Owerweight';
+    if (bmi >= 30) return 'Obese';
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>BMI Calculator</h1>
+        <form>
+          <div>
+            <label>Height</label>
+            <Range
+              onChange={this.heightChange}
+              value={this.state.height}
+            />
+          </div>
+          <div>
+            <label>Weight</label>
+            <Range
+              onChange={this.weightChange}
+              value={this.state.weight}
+            />
+          </div>
+        </form>
+        <br /><br />
+        <Output
+          data={this.state}
+        />
       </div>
     );
   }
---
+++ APPS/React/Projects/04_bodymasscalculator/src/components/Range.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Range extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    }
+  }
+
+  static defaultProps = {
+    min: 0,
+    max: 245,
+    step: 1
+  }
+
+  onChange = (e) => {
+    console.log(e.target.value);
+    this.props.onChange(this.state.value);
+    this.setState({
+      value: e.target.value
+    });
+  }
+  render() {
+    return (
+      <div className="range">
+        <input
+          type="range"
+          value={this.state.value}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
+}
+
+Range.propTypes = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+};
+
+export default Range;
