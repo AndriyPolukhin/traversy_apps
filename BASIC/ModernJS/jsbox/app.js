52ff7e730ef84674f4022ab049fbe21dd76a120e
Andriy Polukhin <andriy.polukhin@gmail.com>
2019-01-05 18:47:05 +0200

    task list with crud

--- BASIC/ModernJS/jsbox/app.js
+++ BASIC/ModernJS/jsbox/app.js
@@ -1,38 +1,44 @@
-let val;
+// Set local storage item;
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// localStorage.setItem('name', 'Andriy');
+// localStorage.setItem('age', '33');
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// session storage
+// sessionStorage.setItem('name', 'Anastasia');
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList;
-val = document.links[0].classList[0];
 
-val = document.images;
+// remove from storage
+// localStorage.removeItem('name')
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// get from storage
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
 
-let scripts = document.scripts;
-let scriptsArray = Array.from(scripts);
+// localStorage.clear();
+// // clear localStorage
+// console.log(name, age);
 
-scriptsArray.forEach((script) => console.log(script.getAttribute('src')));
 
-console.log(val);
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = document.getElementById('task').value;
+  let tasks;
+
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  alert('Task Saved');
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+console.log(tasks);
+
+tasks.forEach((task) => console.log(task))
